7a4a7054ba1e15d99094d355050f5127941e1951
kseniaryabinova <ryabinova.ka@mail.ru>
2021-04-14 16:12:08 +0300

    added bin hash

--- train_scripts/hash/find_treshold.py
+++ train_scripts/hash/find_treshold.py
@@ -10,7 +10,7 @@ df = pd.read_csv('../../dataset/train_fold.csv')
 tmp = df.groupby('label_group').posting_id.agg('unique').to_dict()
 df['target'] = df.label_group.map(tmp)
 
-# df['hash_bin'] = df['image_phash'].apply(lambda x: "{0:08b}".format(int(x, 16)))
+df['hash_bin'] = df['image_phash'].apply(lambda x: "{0:08b}".format(int(x, 16)))
 
 
 def hamming_distance(chaine1, chaine2):
@@ -19,7 +19,7 @@ def hamming_distance(chaine1, chaine2):
 
 def get_simular_hashes(orig_hash, thresh):
     simular_hash_ids = []
-    for i, single_hash in enumerate(df['image_phash']):
+    for i, single_hash in enumerate(df['hash_bin']):
         if hamming_distance(orig_hash, single_hash) < thresh:
             simular_hash_ids.append(df.loc[i]['posting_id'])
     return simular_hash_ids
@@ -33,8 +33,8 @@ def get_metric(col):
     return f1score
 
 
-for th in range(1, 16):
-    df['preds'] = df['image_phash'].parallel_apply(lambda x: get_simular_hashes(x, th))
+for th in range(1, 64):
+    df['preds'] = df['hash_bin'].parallel_apply(lambda x: get_simular_hashes(x, th))
 
     df['f1'] = df.apply(get_metric('preds'), axis=1)
     print(th, df['f1'].mean())
