7b16982405b06458229ce9619814db5890e4b6aa
kseniaryabinova <ryabinova.ka@mail.ru>
2021-04-23 08:59:41 +0300

    попробовала прочитать доку не жопой

--- train_scripts/text/train_loop_acc.py
+++ train_scripts/text/train_loop_acc.py
@@ -16,21 +16,24 @@ from text.train_functions import train_one_epoch
 
 os.environ['WANDB_SILENT'] = 'true'
 
-batch_size = 4
+batch_size = 16
 n_epochs = 20
 max_len = 128
 weight_decay = 0.01
 init_lr = 1e-5
 fold_number = 0
 
-group_name = wandb.util.generate_id()
-wandb.init(project='bert_base', group=group_name, job_type=str(fold_number))
+accelerator = Accelerator()
 
-wandb.config.batch_size = batch_size
-wandb.config.n_epochs = n_epochs
-wandb.config.max_len = max_len
-wandb.config.weight_decay = weight_decay
-wandb.config.init_lr = init_lr
+if accelerator.is_main_process:
+    group_name = wandb.util.generate_id()
+    wandb.init(project='bert_base', group=group_name, job_type=str(fold_number))
+
+    wandb.config.batch_size = batch_size
+    wandb.config.n_epochs = n_epochs
+    wandb.config.max_len = max_len
+    wandb.config.weight_decay = weight_decay
+    wandb.config.init_lr = init_lr
 
 df = pd.read_csv('../../dataset/reliable_validation_tm.csv')
 train_dataset = TextDataset(df, df[df['fold_group'] != 0], max_len=max_len)
@@ -38,13 +41,11 @@ train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuf
 valid_dataset = TextDataset(df, df[df['fold_group'] == 0], max_len=max_len)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-accelerator = Accelerator()
+
 
 configuration = BertConfig(max_position_embeddings=max_len)
 model = BertForSequenceClassification.from_pretrained(
     "bert-base-multilingual-cased", num_labels=df['label_group'].nunique())
-model.to(device)
-model.train()
 
 no_decay = ['bias', 'LayerNorm.weight']
 optimizer_grouped_parameters = [
@@ -62,10 +63,13 @@ best_loss = 0
 for epoch in range(n_epochs):
     train_loss = train_one_epoch(model, train_dataloader, optimizer, accelerator, device)
 
-    wandb.log({'train_loss': train_loss, 'epoch': epoch})
+    accelerator.wait_for_everyone()
 
+    if accelerator.is_main_process:
+        wandb.log({'train_loss': train_loss, 'epoch': epoch})
     if train_loss > best_loss:
         best_loss = train_loss
-        torch.save(model.state_dict(), 'best_bt.pth')
+        accelerator.save(accelerator.unwrap_model(model).state_dict(), 'best_bt.pth')
 
-wandb.finish()
+if accelerator.is_main_process:
+    wandb.finish()
