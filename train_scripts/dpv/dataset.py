9188e179d92972f3cf2b5b37cabfa2d37fab9107
kseniaryabinova <ryabinova.ka@mail.ru>
2021-05-08 16:08:19 +0300

    added small helper and its train loop

--- train_scripts/dpv/dataset.py
+++ train_scripts/dpv/dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import torch
 
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
@@ -7,23 +8,26 @@ from transformers import AutoTokenizer
 def dummy_df():
     df = pd.DataFrame({
         'text': ['kek', 'lol', 'i want to chill'],
-        'label': [1, 2, 3]
+        'label': [0, 1, 2]
     })
     return df
 
 
 class ClassificationDataset(Dataset):
-    def __init__(self, df):
+    def __init__(self, df, max_len):
         self.df = df
+        self.max_len = max_len
         self.tokenizer = AutoTokenizer.from_pretrained(
-            "DeepPavlov/rubert-base-cased-sentence")
+            "DeepPavlov/rubert-base-cased-sentence",
+            max_position_embeddings=max_len)
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, index):
         text = self.df.iloc[index]['text']
-        tokens = self.tokenizer(text, return_tensors='pt', padding=True)
+        tokens = self.tokenizer(text, return_tensors='pt', padding='max_length',
+                                max_length=self.max_len)
         label = self.df.iloc[index]['label']
 
-        return tokens, label
+        return torch.squeeze(tokens.input_ids), label
--- train_scripts/dpv/train.py
+++ train_scripts/dpv/train.py
@@ -5,11 +5,28 @@ from torch.nn import CrossEntropyLoss
 from dpv.dataset import ClassificationDataset, dummy_df
 from dpv.model import DeepPavlovBERT
 
-dataset = ClassificationDataset(dummy_df())
-dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+dataset = ClassificationDataset(dummy_df(), 32)
+dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
 
 model = DeepPavlovBERT(3)
 optimizer = Adam(model.parameters())
 criterion = CrossEntropyLoss()
 
+n_epoch = 10
 
+for epoch in range(n_epoch):
+    mean_loss = 0
+    iter_counter = 0
+
+    for tokens, labels in dataloader:
+        outputs = model(tokens)
+
+        optimizer.zero_grad()
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        mean_loss += loss.item()
+        iter_counter += 1
+
+    print(mean_loss / iter_counter)
